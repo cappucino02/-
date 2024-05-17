ffe707df94f1c6b006f3acc3b7046dd602dad709
May <867120904@qq.com>
2024-05-17 11:43:03 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,360 @@
+
+
+var data = [
+    // 示例数据
+    {
+        chinese: "休克",
+        japanese: "ショック",
+        explanation_chinese: "机体遭受强烈致病因子侵袭下，有效循环血量急剧减少，组织血液灌流量严重不足，引起细胞缺血缺氧，以致各重要生命器官的功能，代谢障碍或结构损害的全身性危重病理过程。",
+        explanation_japanese: "急激な末梢血液循環の不全状態。血圧および体温の低下、意識障害等を来し、重症の場合、脳・心臓・腎臓などの機能障害をひき起こして死に至る。",
+        corresponding_expression_chinese: "在肺炎治疗过程中出现休克状态，发现血液浓缩并出现低白蛋白血症。",
+        corresponding_expression_japanese: "肺炎治療の経過中にショック状態となり，急激に進行する血液濃縮所見と低アルブミン血症を認めた",
+        application_scenario_chinese: "医疗领域，医学诊断。常见因失血、烧伤、感染、 过敏或心源性休克而表现表情淡漠，皮肤湿冷，烦躁不安，血压下降的症状的病人",
+        application_scenario_japanese: "医療分野では医療診断です。出血、火傷、感染症、アレルギー、心因性などが原因です。ショックで表情が薄くなったり、肌が冷たくなったり、イライラしたり、血圧が下がったりといった症状を訴える人もいます。",
+        source: "日本内科学会雑誌，Volume 109 , Issue 4 . 2020. PP 792-797",
+        corresponding_expression_source: "日本内科学会雑誌，Volume 109 , Issue 4 . 2020. PP 792-797"
+   
+    },
+        {
+        chinese: "盗汗",
+        japanese: "寝汗( ねあせ)",
+        explanation_chinese: "盗汗是睡眠中不自主的异常出汗现象，通常在深夜及黎明前明显，而睡醒后就会停止出汗",
+        explanation_japanese: "寝汗は睡眠中の自主的ではない異常な発汗現象であり、通常は深夜や夜明け前に顕著であるが、目が覚めると発汗を停止する",
+        corresponding_expression_chinese: "（你）有盗汗症状吗",
+        corresponding_expression_japanese: "寝汗の症状はありますか。",
+        application_scenario_chinese: "医疗领域，医学诊断。常见于肾阴虚，结核病等患者症状",
+        application_scenario_japanese: "医療分野では医療診断です。腎陰虚、結核などによく見られる症状です。",
+        source:"中华中医药杂志，2021",
+        corresponding_expression_source: "日本医学会医学用语辞典"
+    },
+        {
+        chinese: "过敏",
+        japanese: "アレルギー",
+        explanation_chinese: "以引起组织细胞损伤或生理功能紊乱为主要结局的异常的免疫应答。",
+        explanation_japanese: "免疫系の異常な反応によって引き起こされる身体の反応を指す。",
+        corresponding_expression_chinese: "（你）对什么过敏？",
+        corresponding_expression_japanese: "どんなアレルギーがありますか。",
+        application_scenario_chinese: "医疗领域，医学诊断。因过敏原引起的发作性皮肤瘙痒，出现出血性风团，甚至休克等症状的病人",
+        application_scenario_japanese: "医療分野では医療診断です。アレルゲンによる発作性皮膚痒、出血性風団、ショックなどの症状を呈する方もいらっしゃいます。",
+        source:"石川 昌，2011，第35回日本トキシコロジー学会学術年会，23-23",
+        corresponding_expression_source: "日本医学会医学用语辞典 英和版第三版"
+    },
+            {
+        chinese: "青霉素",
+        japanese: "ペニシリン",
+        explanation_chinese: "由青霉菌中提炼出的抗生素。",
+        explanation_japanese: "青かびの一種が出す抗菌性物質。",
+        corresponding_expression_chinese: "首先必须先进行青霉素皮试。",
+        corresponding_expression_japanese: "まずペニシリン皮試験をしなければなりません。 ",
+        application_scenario_chinese: "生物制药。临床常用抗生素。适用于敏感细菌所致各种感染。",
+        application_scenario_japanese: "バイオ製薬です。臨床では抗生物質が使われます敏感な細菌によるさまざまな感染症に適しています。",
+        source:"平,友恒，大和,谷三郎，藤井,繁弘 ，1950，《ペニシリンその他抗生物質》，抗菌物質のPaper chromatography〔第1報〕",
+        corresponding_expression_source: "日本医学会医学用语辞典 英和版第三版"
+    },
+
+            {
+        chinese: "副作用",
+        japanese: "副作用ふくさよう",
+        explanation_chinese: "指应用治疗量的药物后所出现的治疗目的以外的药理作用。",
+        explanation_japanese: "医薬品の使用に伴って生じた治療目的に沿わない作用全般〔治療目的の作用は「主作用」と呼ぶ。薬効とも呼ぶ〕を指す。",
+        corresponding_expression_chinese: "这药有副作用吗？",
+        corresponding_expression_japanese: "薬の副作用はありませんか",
+        application_scenario_chinese: "医学诊断、药物说明",
+        application_scenario_japanese: "医療診断です。効能書きに多く見られます。",
+        source:"医疗术语 语句集",
+        corresponding_expression_source: "医疗术语 语句集"
+    },
+            {
+        chinese: "抽筋",
+        japanese: "痙攣けいれん",
+        explanation_chinese: "是一种肌肉自发的强直性收缩。",
+        explanation_japanese: "不随意に筋肉が激しく収縮することによって起こる発作",
+        corresponding_expression_chinese: "这里抽筋了",
+        corresponding_expression_japanese: "ここがけいれんします",
+        application_scenario_chinese: "医学诊断",
+        application_scenario_japanese: "医療診断です。",
+        source:"医疗术语 语句集",
+        corresponding_expression_source: "医疗术语 语句集"
+    },
+            {
+        chinese: "残尿感",
+        japanese: "残尿感ざんにょうかん",
+        explanation_chinese: "在人体排尿之后仍然感觉膀胱内部有尿液存在",
+        explanation_japanese: "排尿後に、尿がでること。",
+        corresponding_expression_chinese: "有余尿感",
+        corresponding_expression_japanese: "残尿感があります",
+        application_scenario_chinese: "医学诊断、症状说明",
+        application_scenario_japanese: "医療診断です。症状を説明する時に使われることが多いです。",
+        source:"医疗术语 语句集",
+        corresponding_expression_source: "医疗术语 语句集"
+    },
+                {
+        chinese: "坐药，栓剂",
+        japanese: "座薬ざやく",
+        explanation_chinese: "药物与适宜基质制成的具有一定形状的供人体腔道内给药的固体制剂",
+        explanation_japanese: "医薬品を基剤に均等に混和して一定の形状に成型して、肛門または膣に適用する半固形の外用剤で、体温により溶けるか、軟化するか、又は分泌液で徐々に溶けるもしくは分散されるものと定義されている",
+        corresponding_expression_chinese: "请在晚上临睡前，把栓剂放进肛门里",
+        corresponding_expression_japanese: "夜寝る前に、座薬を入れてください",
+        application_scenario_chinese: "生物制药",
+        application_scenario_japanese: "バイオ製薬です。",
+        source:"医疗术语 语句集",
+        corresponding_expression_source: ""
+    },
+                {
+        chinese: "消毒",
+        japanese: "しょうどく",
+        explanation_chinese: "杀死病原微生物、但不一定能杀死细菌芽孢的方法。",
+        explanation_japanese: "消毒とは、病原性微生物を殺す方法を指しますが、必ずしも細菌の胞子を殺す必要はありません。",
+        corresponding_expression_chinese: "消毒杀死潜在的有害细菌来清洁伤口",
+        corresponding_expression_japanese: "潜在的に有害なバクテリアを殺すことによって傷口をきれいにするために消毒します",
+        application_scenario_chinese: "日常护理",
+        application_scenario_japanese: "日常的な看護するに使われることが多いです。",
+        source:"国家卫健委，WS/T466—2014",
+        corresponding_expression_source: ""
+    },
+                {
+        chinese: "嗜睡",
+        japanese: "眠気",
+        explanation_chinese: "持续或频繁的睡眠欲望和倾向，使人难以保持清醒和专注",
+        explanation_japanese: "絶え間ない、または頻繁な睡眠欲求と傾向により、目を覚まして集中し続けることが困難になる。",
+        corresponding_expression_chinese: "精力不集中，精神疲倦，不自主地入睡、精神不清醒",
+        corresponding_expression_japanese: "集中力の欠如と起きていることができない",
+        application_scenario_chinese: "医学诊断、症状说明",
+        application_scenario_japanese: "医療診断です。症状を説明する時に使われることが多いです。",
+        source:"中华医学杂志, 2023, 103(15) : 1103-1118",
+        corresponding_expression_source: ""
+    },
+                {
+        chinese: "多尿",
+        japanese: "利尿",
+        explanation_chinese: "每天24小时排尿多于2500ml",
+        explanation_japanese: "2500ml以上を24時間排尿",
+        corresponding_expression_chinese: "暂时性生理性多尿和持续性多尿，不可抑制的排尿量增加",
+        corresponding_expression_japanese: "もっと排尿する",
+        application_scenario_chinese: "医疗领域、医学诊断，对应科室为肾内科、内分泌科、泌尿外科、神经内科、精神心理科",
+        application_scenario_japanese: "医療分野では医療診断です。診療科は腎臓内科、内分泌科、泌尿器科、神経内科、精神心理科。",
+        source:"《内分泌与代谢性疾病症状鉴别诊断学》",
+        corresponding_expression_source: ""
+    },
+                {
+        chinese: "高血压",
+        japanese: "高血圧",
+        explanation_chinese: "以体循环动脉血压增高为主要特征，可伴有心、脑、肾等器官的功能或器质性损害的临床综合征。",
+        explanation_japanese: "これは、全身動脈の血圧上昇を特徴とする臨床症候群であり、心臓、脳、腎臓および他の臓器への機能的または器質的損傷を伴うことがある。",
+        corresponding_expression_chinese: "收缩压≥140毫米汞柱，舒张压≥90毫米汞柱",
+        corresponding_expression_japanese: "収縮期血圧≥140mmHg、拡張期血圧≥90mmHg。",
+        application_scenario_chinese: "医疗领域、医学诊断，对应诊断科室为心内科或普通内科",
+        application_scenario_japanese: "医療分野では医療診断です。診療科は心療内科または一般内科です。",
+        source:"中国心血管杂志, 2019, 24(1): 24-56",
+        corresponding_expression_source: ""
+    },
+                {
+        chinese: "内窥镜",
+        japanese: "内視鏡[ないしきょう]",
+        explanation_chinese: "是一种医疗设备，主要用于观察人体内部。",
+        explanation_japanese: "主に人体内部を観察することを目的とした医療機器である。",
+        corresponding_expression_chinese: "是否携带内窥镜照片？",
+        corresponding_expression_japanese: "内視鏡フィルムをもっていますか",
+        application_scenario_chinese: "仪器名称",
+        application_scenario_japanese: "機器名です。",
+        source:"医疗术语 语句集",
+        corresponding_expression_source: "医疗术语 语句集"
+    },
+                {
+        chinese: "注射",
+        japanese: "注射[ちゅうしゃ]",
+        explanation_chinese: "用针头将药物直接注入体内的方法。。",
+        explanation_japanese: "注射針を用いて直接体内に薬剤を注入する投与法。",
+        corresponding_expression_chinese: "将注射针插入静脉。",
+        corresponding_expression_japanese: "注射針を静脈りゅうのなかに刺します。",
+        application_scenario_chinese: "护理操作",
+        application_scenario_japanese: "ケア作業の一種です。",
+        source:"医疗术语语句集",
+        corresponding_expression_source: " 新見正則|著   下肢静脈りゅうを防ぐ・治す "
+    },
+                {
+        chinese: "麻醉",
+        japanese: "麻酔[ますい]",
+        explanation_chinese: "通过药物或其他手段人为地消除疼痛和其他感觉。",
+        explanation_japanese: "薬物などによって人為的に疼痛をはじめとする感覚をなくすことである。",
+        corresponding_expression_chinese: "是否有过因麻醉引起的不良反应？",
+        corresponding_expression_japanese: "麻酔をして何かトラブルがありましたか",
+        application_scenario_chinese: "护理操作",
+        application_scenario_japanese: "ケア作業の一種です。",
+        source:"医疗术语 语句集",
+        corresponding_expression_source: "医疗术语 语句集"
+    },
+                    {
+        chinese: "洗胃",
+        japanese: "胃洗浄（いせんじょう）",
+        explanation_chinese: "当误食或意外摄入对人体有害的物质时，可使用水或生理盐水等清洗液、胃管（在进行洗胃前，先插上气管插管并充气罩囊，因为病人昏迷时可能会发生吸入性肺炎）、活性炭等吸附剂、解毒剂和润滑剂，以方便插管。 在洗胃前，医生会给病人注射胃药，目的是清除残留在胃中未被吸收的物质。",
+        explanation_japanese: "人体に有害な物質を誤食・誤飲したとき、水や生理的食塩水などの洗浄液と胃チューブ（患者が意識を失っている時は誤嚥性肺炎を起こす可能性があるため、気管内チューブを挿管し、カフを膨らませてから胃洗浄を行う）、活性炭などの吸着剤や解毒剤、チューブの挿管を容易にする潤滑剤を用いて、胃に残る未吸収物質を除去する目的で行われる。",
+        corresponding_expression_chinese: "现在，他们应该尽快入院，接受洗胃、静脉输液、透析等治疗。",
+        corresponding_expression_japanese: "今は、できるだけ早く入院させ、	胃洗浄	、点滴、人工透析などの手当てをする。",
+        application_scenario_chinese: "护理操作",
+        application_scenario_japanese: "ケア作業の一種です。",
+        source:"守安　洋子(著)ナースのためのくすりの事典",
+        corresponding_expression_source: "小林路子|著  なにがなんでも！きのこが好き "
+    },
+                    {
+        chinese: "耳鸣",
+        japanese: "耳鳴り",
+        explanation_chinese: "患者在缺乏外部声源的情况下，耳内或颅内产生不成形的异常声幻觉。",
+        explanation_japanese: "実際には音がしていないのにも拘らず、何かが聞こえるように感じる現象",
+        corresponding_expression_chinese: "经常耳鸣",
+        corresponding_expression_japanese: "いつも耳鳴りがします。",
+        application_scenario_chinese: "医学诊断、症状说明",
+        application_scenario_japanese: "医療診断です。症状を説明する時に使われることが多いです。",
+        source:"医疗术语 语句集",
+        corresponding_expression_source: "医疗术语 语句集"
+    },
+
+     {
+        chinese: "牙科低速手机/牙科慢速手机/慢手机",
+        japanese: "低速歯科用スケーラー",
+        explanation_chinese: "在牙科治疗领域中，为了去除牙面和牙周桶等产生的牙结石和菌斑等而使用的缩放剂的刀片，用砂轮手动研磨时使用的辅助工具。",
+        explanation_japanese: "歯科治療分野で歯面や歯周ボケット等に発生する歯石やプラーク等を除去するために使用されるスケーラーの刃を砥石によって手動で研磨するに際して使用される補助具です。",
+        corresponding_expression_chinese: "把慢手机拿给我",
+        corresponding_expression_japanese: "低速歯科用スケーラー、お願い。",
+        application_scenario_chinese: "仪器名称",
+        application_scenario_japanese: "機器名です。手術中に使われることが多いです。",
+        source:"日本医学会医学用语辞典 英和版第三版",
+        corresponding_expression_source: ""
+    },
+
+    {
+        chinese: "抗生素药膏",
+        japanese: "抗生物質クリーム",
+        explanation_chinese: "用于外用的抗生素药物，通常用于治疗表皮的细菌感染",
+        explanation_japanese: "外用抗生物質は、主に表皮の細菌感染を治療するために使用される。",
+        corresponding_expression_chinese: "如果有轻微的皮肤感染，可以使用抗生素药膏。",
+        corresponding_expression_japanese: "軽い皮膚感染がある場合、抗生物質クリームを使用できます。",
+        application_scenario_chinese: "日常皮肤护理和医疗",
+        application_scenario_japanese: "日常の皮膚ケアと医療で使用されます。",
+        source: "现代医疗手册",
+        corresponding_expression_source: "日本医療大辞典"
+    }
+    
+];
+
+// 搜索函数
+function search() {
+    var input = document.getElementById('search-input').value.trim();  // 获取并清理输入框的值
+    var languageSelect = document.getElementById('language-select').value;  // 获取选择的语言
+    var resultsDiv = document.getElementById('results');  // 获取结果展示区域的元素
+    
+    resultsDiv.innerHTML = '';  // 清空之前的搜索结果
+
+
+    if (input === '') {
+        return;  // 如果输入为空，直接返回，不执行后续搜索操作
+    }
+
+    // 创建一个正则表达式，'i'标志表示不区分大小写
+    var regex = new RegExp(input, 'i');
+
+    // 过滤数据，只保留匹配正则表达式的项
+    var filteredData = data.filter(function(item) {
+        // 根据语言选择，匹配中文或日文内容
+        return languageSelect === 'chinese' ? 
+               regex.test(item.chinese) || regex.test(item.corresponding_expression_chinese) : 
+               regex.test(item.japanese) || regex.test(item.corresponding_expression_japanese);
+    });
+
+    // 遍历过滤后的数据，为每个项创建搜索结果元素
+    filteredData.forEach(function(item) {
+        var resultItemDiv = document.createElement('div');
+        resultItemDiv.className = 'result-item';  // 设置结果项的类名
+        
+        // 创建并设置词条标题元素
+        var termTitle = document.createElement('h3');
+        termTitle.className = 'term-title';
+        termTitle.textContent = languageSelect === 'chinese' ? `${item.chinese} - ${item.japanese}` : `${item.japanese} - ${item.chinese}`;
+        resultItemDiv.appendChild(termTitle);
+        
+        // 创建解释、相应表达和应用场景的展示部分
+        resultItemDiv.appendChild(createSection('解释', item.explanation_chinese, item.explanation_japanese, languageSelect, item.source));
+        resultItemDiv.appendChild(createSection('应用场景', item.application_scenario_chinese, item.application_scenario_japanese, languageSelect));
+        resultItemDiv.appendChild(createSection('相应表达', item.corresponding_expression_chinese, item.corresponding_expression_japanese, languageSelect, item.corresponding_expression_source));
+        
+        resultsDiv.appendChild(resultItemDiv);  // 将结果项添加到结果展示区域
+    });
+}
+
+function createSection(title, chineseContent, japaneseContent, languageSelect, source) {
+    var sectionDiv = document.createElement('div');
+    sectionDiv.className = 'result-section';
+
+    var titleSpan = document.createElement('span');
+    titleSpan.className = 'result-section-title';
+    titleSpan.textContent = title + ': ';
+    sectionDiv.appendChild(titleSpan);
+
+    var contentDiv = document.createElement('div');
+    contentDiv.className = 'result-section-content';
+    sectionDiv.appendChild(contentDiv);
+
+    var primaryContent = document.createElement('p');
+    var secondaryContent = document.createElement('p');
+
+    // 根据用户选择的语言显示顺序
+    if (languageSelect === 'chinese') {
+        primaryContent.textContent = chineseContent;
+        secondaryContent.textContent = japaneseContent;
+    } else {
+        primaryContent.textContent = japaneseContent;
+        secondaryContent.textContent = chineseContent;
+    }
+
+    contentDiv.appendChild(primaryContent);
+    contentDiv.appendChild(secondaryContent);
+
+    // 如果存在出处信息，添加悬停文本
+    if (source) {
+        var sourceTooltip = createTooltip(source);
+        secondaryContent.appendChild(sourceTooltip);
+    }
+
+    return sectionDiv;
+}
+
+// 创建悬停文本的函数
+function createTooltip(text) {
+    var tooltipSpan = document.createElement('span');
+    tooltipSpan.className = 'tooltip';
+    tooltipSpan.textContent = ' [来源]';
+
+    var tooltipText = document.createElement('span');
+    tooltipText.className = 'tooltiptext';
+    tooltipText.textContent = text;
+
+    tooltipSpan.appendChild(tooltipText);
+
+    return tooltipSpan;
+}
+
+function showSuggestions(inputText) {
+    if (inputText.length === 0) {
+        document.getElementById('suggestions-box').innerHTML = '';
+        return;
+    }
+
+    var suggestions = data.filter(item => item.chinese.startsWith(inputText) || item.japanese.startsWith(inputText));
+    var suggestionsHTML = suggestions.map(item => `<p onclick="selectSuggestion('${item.chinese}')">${item.chinese} - ${item.japanese}</p>`).join('');
+    document.getElementById('suggestions-box').innerHTML = suggestionsHTML;
+}
+
+function selectSuggestion(value) {
+    document.getElementById('search-input').value = value;
+    search();  // 触发搜索函数
+    document.getElementById('suggestions-box').innerHTML = '';  // 清空建议列表
+}
+
+
+// 当页面加载完成后，绑定搜索按钮的点击事件到搜索函数
+document.addEventListener('DOMContentLoaded', function() {
+    var searchButton = document.getElementById('search-button');
+    if (searchButton) {
+        searchButton.addEventListener('click', search);
+    }
+});
